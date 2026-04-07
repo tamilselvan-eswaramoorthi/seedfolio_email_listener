60d44c8db074f12dad9547e8728c5d6bcabd99df
tamilselvan-eswaramoorthi <eswaramoorthi.tamilselvan@gmail.com>
2026-04-07 10:51:47 +0530

    error handling in db

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,8 @@
 from .models import User, Transaction, Holdings, GoogleOAuthToken, Stock, IPO, EmailTasks, Demerger, StockSplit, Bonus  # noqa: F401
 from .database import Database
 
-db_handler = Database() 
+try:
+    db_handler = Database()
+except Exception as e:
+    print(f"Error initializing database: {e}")
+    db_handler = None
